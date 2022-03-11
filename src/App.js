045a8d55306366a5b4dc8512d7ada6f54a262596
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-03-12 01:41:59 +0300

    sidebar commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 
 import './App.css';
+import SideBar from './components/sidebar/SideBar';
 import Topbar from './components/topbar/Topbar';
 
 function App() {
   return (
     <div>
       <Topbar/>
+      <div className="container">
+        <SideBar/>
+        <div className="others">Other pages</div>
+      </div>
+      
+      
     </div>
   );
 }
--- src/components/sidebar/SideBar.jsx
+++ src/components/sidebar/SideBar.jsx
@@ -1,7 +1,33 @@
+import { LineStyle, Timeline, TrendingUp } from '@material-ui/icons'
 import './sidebar.css'
 
 export default function SideBar() {
   return (
-    <div>SideBar</div>
+    <div>
+        <div className="sidebar">
+            <div className="sidebarWrapper">
+                <div className="sidebarMenu">
+                    <h3 className="sidebarTitle">
+                        Dashboard
+                    </h3>
+                    <ul className="sidebarList">
+                        <li className="sidebarListItem">
+                            <LineStyle/>
+                            Home
+                        </li>
+                        <li className="sidebarListItem">
+                            <Timeline/>
+                            Analytics
+                        </li>
+                        <li className="sidebarListItem">
+                            <TrendingUp/>
+                            Sales
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+    </div>
   )
 }
