0decbef63dac60afd14703e4fc84a279f50cbb93
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-03-12 01:08:43 +0300

    topbar commit

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,29 @@
+import { Language, NotificationsNone, Settings } from "@material-ui/icons";
+import React from "react";
+import "./topbar.css";
+
+export default function Topbar() {
+  return (
+    <div className="topbar">
+      <div className="topbarWrapper">
+        <div className="topLeft">
+          <span className="logo">Admin Sensei</span>
+        </div>
+        <div className="topRight">
+          <div className="topbarIconContainer">
+            <NotificationsNone />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Language />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Settings />
+          </div>
+          <img src="/r_sensei.png" alt="" className="topAvatar" />
+        </div>
+      </div>
+    </div>
+  );
+}
