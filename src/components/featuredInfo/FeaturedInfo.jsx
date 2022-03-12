65a87533df08e0c2059897c7b94ab959de646510
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-03-12 11:54:38 +0300

    featured Item commit

--- src/components/featuredInfo/FeaturedInfo.jsx
+++ src/components/featuredInfo/FeaturedInfo.jsx
@@ -1,4 +1,4 @@
-import { ArrowDownward } from '@material-ui/icons'
+import { ArrowDownward, ArrowUpward } from '@material-ui/icons'
 import './featuredInfo.css'
 
 export default function FeaturedInfo() {
@@ -12,6 +12,30 @@ export default function FeaturedInfo() {
                 <span className="featuredMoney">$2,415</span>
                 <span className="featuredMoneyRate">
                     -11.5 <ArrowDownward/>
+                </span>
+            </div>
+            <span className="featuredSub"> Compared to last month</span>
+        </div>
+        <div className="featuredItem">
+            <span className="featuredTitle">
+                Sales
+            </span>
+            <div className="featuredMoneyContainer">
+                <span className="featuredMoney">$4,415</span>
+                <span className="featuredMoneyRate">
+                    -1.5 <ArrowDownward/>
+                </span>
+            </div>
+            <span className="featuredSub"> Compared to last month</span>
+        </div>
+        <div className="featuredItem">
+            <span className="featuredTitle">
+                Revenue
+            </span>
+            <div className="featuredMoneyContainer">
+                <span className="featuredMoney">$2,415</span>
+                <span className="featuredMoneyRate">
+                    +1.7 <ArrowUpward/>
                 </span>
             </div>
             <span className="featuredSub"> Compared to last month</span>
