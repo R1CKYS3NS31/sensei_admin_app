93027bb665b2690409e85747a3ecb5009ecf8d33
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-03-13 13:04:09 +0300

    widget small commit

--- src/components/views/widgetSm/WidgetSM.jsx
+++ src/components/views/widgetSm/WidgetSM.jsx
@@ -1,7 +1,34 @@
-import './widgetSm.css'
+import { Visibility } from "@material-ui/icons";
+import "./widgetSm.css";
 
 export default function WidgetSM() {
   return (
-    <div className='widgetSm'>small</div>
-  )
+    <div className="widgetSm">
+      <span className="widgetSmTitle">New Join Members</span>
+      <ul className="widgetSmList">
+        <li className="widgetSmListItem">
+          <img src="/sensei.png" alt="User Img" className="widgetSmImg" />
+          <div className="widgetSmUser">
+            <div className="widgetSMUsername">Ricky Sensei</div>
+            <div className="widgetSmUserTitle">Software Engiineer</div>
+          </div>
+          <button className="widgetSmButton">
+              <Visibility/>
+              Display
+          </button>
+        </li>
+        <li className="widgetSmListItem">
+          <img src="/sensei.png" alt="User Img" className="widgetSmImg" />
+          <div className="widgetSmUser">
+            <div className="widgetSMUsername">Ricky Sensei</div>
+            <div className="widgetSmUserTitle">Software Engiineer</div>
+          </div>
+          <button className="widgetSmButton">
+              <Visibility/>
+              Display
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
 }
