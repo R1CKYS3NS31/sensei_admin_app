b206e709d5c8ab75734a9bd6533a7c98757e9821
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-03-13 14:57:44 +0300

    users commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './components/views/pages/home/Home';
 import SideBar from './components/views/sidebar/SideBar';
 import Topbar from './components/views/topbar/Topbar';
 import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
+import UserList from './components/views/pages/userList/UserList';
 
 
 function App() {
@@ -13,7 +14,13 @@ function App() {
       <div className="container">
         <SideBar/>
         <Switch>
-           <Home/>
+          <Route path={"/"} exact>
+            <Home/>
+          </Route>
+          <Route path={"users"}>
+            <UserList/>
+          </Route>
+           
         </Switch>
        
       </div>
