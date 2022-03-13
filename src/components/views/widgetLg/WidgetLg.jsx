9e4c62b924bc1c17bfdc04e0107db3d3397507c9
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-03-13 14:02:29 +0300

    widget large commit

--- src/components/views/widgetLg/WidgetLg.jsx
+++ src/components/views/widgetLg/WidgetLg.jsx
@@ -1,7 +1,83 @@
-import React from 'react'
+import './widgetLg.css'
 
 export default function WidgetLg() {
+  const Button = ({ type }) => {
+    return <button className={"widgetLgButton" + type}>{type}</button>;
+  };
+
   return (
-    <div className='widgetSm'>large</div>
-  )
+    <div className="widgetSm">
+      <h3 className="widgetLgTitle">Latest Transactions</h3>
+      <tanle className="widgetLgTable">
+        <tr className="widgetLgTr">
+          <th className="widgetLgTh">Customer</th>
+          <th className="widgetLgTh">Date</th>
+          <th className="widgetLgTh">Amount</th>
+          <th className="widgetLgTh">Status</th>
+        </tr>
+        <tr className="widgetLgTr">
+          <td className="widgetLgUser">
+            <img src="sensei.png" alt="" className="widgetLgImg" />
+            <span className="widgetLgName">Sensei</span>
+          </td>
+          <td className="widgetLgDate">13 Mar 2022</td>
+          <td className="widgetLgAmount">Ksh. 300.00</td>
+          <td className="widgetLgStatus">
+            <Button type="Approved" />
+          </td>
+        </tr>
+        <tr className="widgetLgTr">
+          <th className="widgetLgTh">Customer</th>
+          <th className="widgetLgTh">Date</th>
+          <th className="widgetLgTh">Amount</th>
+          <th className="widgetLgTh">Status</th>
+        </tr>
+        <tr className="widgetLgTr">
+          <td className="widgetLgUser">
+            <img src="sensei.png" alt="" className="widgetLgImg" />
+            <span className="widgetLgName">Sensei</span>
+          </td>
+          <td className="widgetLgDate">13 Mar 2022</td>
+          <td className="widgetLgAmount">Ksh. 300.00</td>
+          <td className="widgetLgStatus">
+            <Button type="Declined"/>
+          </td>
+        </tr>
+        <tr className="widgetLgTr">
+          <th className="widgetLgTh">Customer</th>
+          <th className="widgetLgTh">Date</th>
+          <th className="widgetLgTh">Amount</th>
+          <th className="widgetLgTh">Status</th>
+        </tr>
+        <tr className="widgetLgTr">
+          <td className="widgetLgUser">
+            <img src="sensei.png" alt="" className="widgetLgImg" />
+            <span className="widgetLgName">Sensei</span>
+          </td>
+          <td className="widgetLgDate">13 Mar 2022</td>
+          <td className="widgetLgAmount">Ksh. 300.00</td>
+          <td className="widgetLgStatus">
+            <Button type="Pending" />
+          </td>
+        </tr>
+        <tr className="widgetLgTr">
+          <th className="widgetLgTh">Customer</th>
+          <th className="widgetLgTh">Date</th>
+          <th className="widgetLgTh">Amount</th>
+          <th className="widgetLgTh">Status</th>
+        </tr>
+        <tr className="widgetLgTr">
+          <td className="widgetLgUser">
+            <img src="sensei.png" alt="" className="widgetLgImg" />
+            <span className="widgetLgName">Sensei</span>
+          </td>
+          <td className="widgetLgDate">13 Mar 2022</td>
+          <td className="widgetLgAmount">Ksh. 300.00</td>
+          <td className="widgetLgStatus">
+            <Button type="Approved" />
+          </td>
+        </tr>
+      </tanle>
+    </div>
+  );
 }
