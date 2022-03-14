b6948e326f34a721b786fac7a5c5b2fb160f19be
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-03-14 12:29:55 +0300

    user commit

--- src/components/views/pages/user/User.jsx
+++ src/components/views/pages/user/User.jsx
@@ -1,7 +1,25 @@
-import './user.css'
+import "./user.css";
 
 export default function User() {
   return (
-    <div className='user'>User</div>
-  )
+    <div className="user">
+      <div className="userTitleContainer">
+        <h1 className="userTitle">Edit User</h1>
+        <button className="userAddButton">Create</button>
+      </div>
+      <div className="userContainer">
+        <div className="userShow">
+          <div className="userShowTop">
+            <img src="/sensei.png" alt="user img" className="userShowImg" />
+            <div className="userShowTopTitle">
+              <span className="userShowUsername">Ricky Sensei</span>
+              <span className="userShowUserTitle">Software Engineer</span>
+            </div>
+          </div>
+          <div className="userShowBottom"></div>
+        </div>
+        <div className="userUpdate"></div>
+      </div>
+    </div>
+  );
 }
