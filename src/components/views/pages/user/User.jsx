208315a745ebc76cce1aae1cb9ba44a17bc978df
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-03-14 13:26:01 +0300

    user update commit

--- src/components/views/pages/user/User.jsx
+++ src/components/views/pages/user/User.jsx
@@ -18,7 +18,7 @@ export default function User() {
             </div>
           </div>
           <div className="userShowBottom">
-            <spna className="userShowTitle">Account Details</spna>
+            <span className="userShowTitle">Account Details</span>
             <div className="userShowInfo">
               <PermIdentity className="userShowIcon" />
               <span className="userShowIfoTitle">rickysensei</span>
@@ -27,6 +27,7 @@ export default function User() {
               <CalendarToday className="userShowIcon" />
               <span className="userShowIfoTitle">12.07.2002</span>
             </div>
+            <span className="userShowTitle">Contact Details</span>
             <div className="userShowInfo">
               <PhoneAndroid className="userShowIcon" />
               <span className="userShowIfoTitle">+254712 345 678</span>
@@ -35,13 +36,40 @@ export default function User() {
               <MailOutline className="userShowIcon" />
               <span className="userShowIfoTitle">rickysensei@gmail.com</span>
             </div>
+            <span className="userShowTitle">Address Details</span>
             <div className="userShowInfo">
               <LocationSearching className="userShowIcon" />
               <span className="userShowIfoTitle">Nakuru City | Kenya</span>
             </div>
           </div>
         </div>
-        <div className="userUpdate"></div>
+        <div className="userUpdate">
+          <span className="userUpdateTitle">Edit</span>
+          <form className="userUpdateForm">
+            <div className="userUpdateLeft">
+              <div className="userUpdateItem">
+                <label htmlFor="username">Username</label>
+                <input type="text" className="userUpdateInput" placeholder="username"/>
+              </div>
+              <div className="userUpdateItem">
+                <label htmlFor="username">Full Name</label>
+                <input type="text" className="userUpdateInput" placeholder="full name"/>
+              </div>
+              <div className="userUpdateItem">
+                <label htmlFor="username">Email</label>
+                <input type="text" className="userUpdateInput" placeholder="email@gmail.com"/>
+              </div>
+              <div className="userUpdateItem">
+                <label htmlFor="username">Phone</label>
+                <input type="telephone" className="userUpdateInput" placeholder="+254700 000 000"/>
+              </div>
+              <div className="userUpdateItem">
+                <label htmlFor="username">Address</label>
+                <input type="text" className="userUpdateInput" placeholder="City,Country"/>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
