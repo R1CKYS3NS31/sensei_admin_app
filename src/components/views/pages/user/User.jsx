c6ff25c70c809cdaf6a5a85202b70d0064f80a4a
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-03-14 12:47:40 +0300

    user info commit

--- src/components/views/pages/user/User.jsx
+++ src/components/views/pages/user/User.jsx
@@ -1,3 +1,4 @@
+import { CalendarToday, LocationSearching, MailOutline, PermIdentity, PhoneAndroid } from "@material-ui/icons";
 import "./user.css";
 
 export default function User() {
@@ -16,7 +17,29 @@ export default function User() {
               <span className="userShowUserTitle">Software Engineer</span>
             </div>
           </div>
-          <div className="userShowBottom"></div>
+          <div className="userShowBottom">
+            <spna className="userShowTitle">Account Details</spna>
+            <div className="userShowInfo">
+              <PermIdentity className="userShowIcon" />
+              <span className="userShowIfoTitle">rickysensei</span>
+            </div>
+            <div className="userShowInfo">
+              <CalendarToday className="userShowIcon" />
+              <span className="userShowIfoTitle">12.07.2002</span>
+            </div>
+            <div className="userShowInfo">
+              <PhoneAndroid className="userShowIcon" />
+              <span className="userShowIfoTitle">+254712 345 678</span>
+            </div>
+            <div className="userShowInfo">
+              <MailOutline className="userShowIcon" />
+              <span className="userShowIfoTitle">rickysensei@gmail.com</span>
+            </div>
+            <div className="userShowInfo">
+              <LocationSearching className="userShowIcon" />
+              <span className="userShowIfoTitle">Nakuru City | Kenya</span>
+            </div>
+          </div>
         </div>
         <div className="userUpdate"></div>
       </div>
